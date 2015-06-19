53a9c93cbe225fdf243204a1737de28fbedd486e
Eugen Martin <eugeniusmartinus@gmail.com>
2015-06-19 10:08:00 +0200

    add config & base funnel task

--- lib/lita/handlers/mixpanel.rb
+++ lib/lita/handlers/mixpanel.rb
@@ -1,6 +1,27 @@
 module Lita
   module Handlers
     class Mixpanel < Handler
+
+      def self.default_config(config)
+        config.api_key = nil
+        config.api_secret = nil
+        config.funnels = []
+      end
+
+      route %r{(mixpanel funnels)}i,
+        :funnels,
+        command: true,
+        help: {
+          "mixpanel funnels" => "gets recent funnels performance from mixpanel"}
+
+      def funnels(response)
+        response.reply "Configured funnels: #{funnel_ids}"
+      end
+
+      def funnel_ids
+        Lita.config.handlers.mixpanel.funnels
+      end
+
     end
 
     Lita.register_handler(Mixpanel)
